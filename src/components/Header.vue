f7fce53ec486a55e7e769d5e015ebfb2a9544ce9
ous776 <90853243+ous776@users.noreply.github.com>
2023-01-18 20:28:00 +0200

    update

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <header>
+      <h1>{{title}}</h1>  
+      <Button @show-addtask="$emit('show-add')" :text="showAtask ? 'Close' : 'Add Task'" :color="showAtask ? 'red' : 'green'"/>
+    </header>
+</template>
+
+
+<script>
+ /* eslint-disable */
+
+ import Button from './Button'
+export default {
+
+    name: 'Header',
+    props: {
+        title: String,
+        showAtask: Boolean,
+    },
+
+    components: {
+        Button
+    }
+}
+</script>
+
+<style scoped>
+header {
+    display:flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+
+
+window.axios = require('axios');
+
+createApp(App).mount('#app')
